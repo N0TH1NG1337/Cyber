78c32619ebd9894ecbeb8aec48e9cb81a735c5a9
N0TH1NG <87660281+N0TH1NG1337@users.noreply.github.com>
2024-02-09 09:29:49 +0200

    Create c_protocol_26.py

---
+++ Common Protocol/c_protocol_26.py
@@ -0,0 +1,58 @@
+"""
+
+Protocol 2.6    .py
+
+last update:    09/02/2024
+
+"""
+
+#  region Libraries
+
+from c_protocol import *
+
+#  endregion
+
+
+#  region class protocol
+
+class c_protocol_26(c_protocol, ABC):
+
+    def __init__(self):
+        super().__init__()
+
+    def create_request(self, cmd: str, args: str) -> str:
+        """
+        Creates a request message by formatting the command.
+        """
+        return format_data(cmd)
+
+    def create_response(self, cmd: str, args: str, skt: socket) -> str:
+        """
+        Create valid response information, will be sent by server,
+        with length field. In case unsupported request "Non-supported cmd" will be sent back
+        """
+        response = "None Supported CMD"
+
+        valid_cmds = {
+            "TIME": self._get_time,
+            "RAND": self._get_random,
+            "NAME": self._get_name
+        }
+
+        if cmd in valid_cmds:
+            response = valid_cmds[cmd]()
+
+        write_to_log(f"  Protocol 2.6  · response to client : {response} ")
+
+        return format_data(response)
+
+    def _get_time(self) -> str:
+        return str(datetime.now())
+
+    def _get_name(self) -> str:
+        return str(socket.gethostname())
+
+    def _get_random(self) -> str:
+        return str(random.randint(1, 10))
+
+#  endregion
